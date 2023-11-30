77d63b8be409bf38ace9de18cf69c3d86c0114c8
kaelecord <99698416+kaelecord@users.noreply.github.com>
2023-11-30 11:00:23 -0500

    correct column descriptions

--- Home.py
+++ Home.py
@@ -33,16 +33,16 @@ with dataset:
     st.caption("Note: the num_talked_about is not in the original dataset.")
     
     st.subheader("Column Descriptions")
-    st.markdown('''- season_id: Unique ID for season number  
-- episode_id: Unique ID for episode number within a season
-- scene_id: Unique ID for scene number within an epsiode  
-- utterace_id: Unique ID for an utterance number within an scene  
-- speaker: name of character who speaks the line   
-- tokens: list of lists - outer list for each sentence within a line - inner list for each word or punction within a sentence.  
-- transcript: tokens joined to form line spoken by speaker for the utterance  
-- emotion: emotion of line determined by results of [Emory NLP Project](https://github.com/emorynlp/emotion-detection)  
-- character_entities: characters spoken about within the utterance as determined by [Emory NLP Project](https://github.com/emorynlp/character-identification)  
-- num_talked_about: integer corresponding to number length of character_entities list''')
+    st.markdown('''-   season_id: Unique ID for season number
+-   episode_id: Unique ID for episode number within a season
+-   scene_id: Unique ID for scene number within an episode
+-   utterace_id: Unique ID for an utterance number within a scene
+-   speaker: name of character who speaks the line
+-   tokens: list of lists - outer list for each sentence within a line - inner list for each word or punctuation within a sentence.
+-   transcript: tokens joined to form a line spoken by a speaker for the utterance
+-   emotion: emotion of line determined by results of  [Emory NLP Project](https://github.com/emorynlp/emotion-detection)
+-   character_entities: characters spoken about within the utterance as determined by  [Emory NLP Project](https://github.com/emorynlp/character-identification)
+-   num_talked_about: integer corresponding to number length of character_entities list''')
     
     st.subheader("Data Description")
     fig = go.Figure()
